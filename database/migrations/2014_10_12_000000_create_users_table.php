48cfaa067853d777af597739345a0cbf25186aed
WormStudy <fui288062@gmail.com>
2025-01-16 16:00:54 +0700

    schema database clear

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text( column: 'address' );
+            $table->string('status')->default( value: 'member');
             $table->rememberToken();
             $table->timestamps();
         });
